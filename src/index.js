0aaf1c6f5070cb7401b3ca35196b8a2030455fab
vorgnr <arnaud.cavat@gmail.com>
2015-07-01 08:55:45 +0200

    Improving game loop & disable deltatime

--- src/index.js
+++ src/index.js
@@ -13,27 +13,21 @@
     var Camera = require('./camera');
     var map = require('./maps').first;
     var Updater = require('./updater');
-    var looper = require('./looper')();
-    
+    var Looper = require('./looper');
+ 
     var drawer = new Drawer(context, uiContext);
     var hero = new Hero(arenaCanvas.width / 2, arenaCanvas.height / 2);
     var camera = new Camera(0, 0, arenaCanvas.width, arenaCanvas.height);
     var updater = new Updater(map, hero, camera);
+    var looper = new Looper();
+    
     var heroes = {};
-
-    var deltaTime, lastframetime = 0.016;
-    var elapsedTime = 1;
     
     var main = function() {
-        var start = new Date().getTime();
-        elapsedTime = start - lastframetime;
-        deltaTime = elapsedTime / 1000.0;
-        lastframetime = start;
-        
         var keysDown = inputs.getKeysDown();
         camera.init();
         drawer.clear(hero.x, hero.y);
-        updater.update(keysDown, deltaTime);
+        updater.update(keysDown, 1);
         updater.updateOtherHeroes(heroes);
         if (hero.isMovingStateChanged)
             socket.emit('updateHero', hero);
@@ -45,11 +39,10 @@
         drawer.drawHeroes(heroes, hero.image);
         
         drawer.debug(hero.x + " : " + hero.y);
-        drawer.debug(elapsedTime + " ms");
-        drawer.debug(deltaTime);
+        drawer.debug(this.deltaTime);
     };
 
-    looper.start(main);
+    looper.start(main, new Date().getTime());
 
     socket.on('updatedHero', function(data) {
         heroes[data.id] = data.hero;
--- src/looper.js
+++ src/looper.js
@@ -1,42 +1,41 @@
 'use strict';
 
-module.exports = function() {
-    var animFrame =   window.requestAnimationFrame ||
-                window.webkitRequestAnimationFrame ||
-                window.mozRequestAnimationFrame    ||
-                window.oRequestAnimationFrame      ||
-                window.msRequestAnimationFrame     ||
-                null ;
+function Looper() {}
 
-    var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-    
-    var animate = function(task) {
-        if (animFrame !== null) {
-            if (isFirefox) {
-                var recursiveAnim = function() {
-                    task();
-                    animFrame();
-                };
+(function () {
 
-                window.addEventListener("MozBeforePaint", recursiveAnim, false);
-                animFrame();
-            } else {
-                var recursiveAnim = function() {
-                    task();
-                    animFrame(recursiveAnim);
-                };
+    var frameTime = 60/1000;
+    var lastTime = 0;
+    var vendors = [ 'ms', 'moz', 'webkit', 'o' ];
 
-                animFrame(recursiveAnim);
-            }
-        } else {
-            var ONE_FRAME_TIME = 1000.0 / 30.0 ;
-            setInterval(task, ONE_FRAME_TIME);
-        }
-    };
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++ x ) {
+        window.requestAnimationFrame = window[ vendors[ x ] + 'RequestAnimationFrame' ];
+        window.cancelAnimationFrame = window[ vendors[ x ] + 'CancelAnimationFrame' ] || window[ vendors[ x ] + 'CancelRequestAnimationFrame' ];
+    }
 
-    return {
-        start: function(task) {
-            return animate(task);
-        }
-    };
+    if (!window.requestAnimationFrame ) {
+        window.requestAnimationFrame = function (callback, element ) {
+            var currTime = Date.now(), timeToCall = Math.max( 0, frameTime - ( currTime - lastTime));
+            var id = window.setTimeout(function() { callback( currTime + timeToCall ); }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    }
+
+    if ( !window.cancelAnimationFrame ) {
+        window.cancelAnimationFrame = function (id) { clearTimeout(id); };
+    }
+
+}());
+
+Looper.prototype.start = function(callback, time) {
+    this.deltaTime = this.lastFrametime ? ( (time - this.lastframetime)/1000.0) : 0.016;
+    this.lastframetime = time;
+    if (!this.callback)
+        this.callback = callback;
+        
+    this.callback();
+    this.id = window.requestAnimationFrame(this.start.bind(this));
 };
+
+module.exports = Looper;
